48ce5b8f5fcccfcc4d190b17258c8f4193470b04
gaurdianaq <gaurdianaq@protonmail.com>
2021-04-12 13:23:32 -0400

    More stuff

--- servers/server_mariadb/src/main/java/org/gaurdianaq/mariadbserver/Main.java
+++
@@ -1,9 +0,0 @@
-package org.gaurdianaq.mariadbserver;
-
-public final class Main {
-    public static void main(String[] argv) {
-        Test test = new Test(5);
-        System.out.println("The cheese is here!");
-        System.out.println(test);
-    }
-}
